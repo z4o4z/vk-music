dc504abaed636334ec2ea7ca13a4883265fe8e72
Evgeniy Zaitsev <overhawlin@gmail.com>
2016-06-15 19:03:30 +0300

    add myAudio container

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import React, {Component, PropTypes} from 'react';
-import {Router, Route} from 'react-router';
+import {Router, Route, IndexRoute} from 'react-router';
 
 import App from '../containers/App/App';
+import MyAudio from '../containers/MyAudio/MyAudio';
 
 export default class MyRouter extends Component {
   static propTypes = {
@@ -12,6 +13,7 @@ export default class MyRouter extends Component {
     return (
       <Router history={this.props.history}>
         <Route path="/" component={App}>
+          <IndexRoute component={MyAudio}/>
         </Route>
       </Router>
     );
