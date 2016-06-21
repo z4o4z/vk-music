995861e606e4ddf65c8217f12d64253e2f9d432b
Evgeniy Zaitsev <overhawlin@gmail.com>
2016-06-21 18:57:53 +0300

    Add AudioList Component

--- src/containers/MyAudio/MyAudio.js
+++ src/containers/MyAudio/MyAudio.js
@@ -1,12 +1,11 @@
-import React, {Component, PropTypes} from 'react';
+import {PropTypes} from 'react';
 import {connect} from 'react-redux';
-import ReactList from 'react-list';
 
 import {getMyAudio} from '../../actions/audio';
 
-import Loader from '../../components/Loader/Loader';
+import AudioList from '../../components/AudioList/AudioList';
 
-class MyAudio extends Component {
+class MyAudio extends AudioList {
   static propTypes = {
     audios: PropTypes.array.isRequired,
     audiosLoading: PropTypes.bool.isRequired,
@@ -14,39 +13,8 @@ class MyAudio extends Component {
     getMyAudio: PropTypes.func.isRequired
   };
 
-  constructor(props) {
-    super(props);
-
-    this.renderItem = this.renderItem.bind(this);
-  }
-
   componentWillMount() {
     this.props.getMyAudio(0, 100);
-  }
-
-  renderItem(index, key) {
-    return <div key={key}>{this.props.audios[index].title}</div>;
-  }
-
-  getLoader() {
-    if (!this.props.audiosLoading) {
-      return null;
-    }
-
-    return <Loader />;
-  }
-
-  render() {
-    return (
-      <div>
-        <ReactList
-          itemRenderer={this.renderItem}
-          length={this.props.audios.length}
-          type="uniform"
-          />
-        {this.getLoader()}
-      </div>
-    );
   }
 }
 
