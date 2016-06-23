c8e499fa70c41268193030b19c3a445e8afdc1f2
Evgeniy Zaitsev <overhawlin@gmail.com>
2016-06-23 19:03:59 +0300

    remove old constnant

--- src/constants/ui.js
+++ src/constants/ui.js
@@ -1,6 +1,5 @@
 export const UI_LEFT_MENU_OPEN = 'UI_LEFT_MENU_OPEN';
 
 export const UI_SHOW_LOADER = 'UI_SHOW_LOADER';
-export const UI_HIDE_LOADER = 'UI_HIDE_LOADER';
 
-export const UI_HEADER_HEIGHT = 64;
+export const UI_HIDE_LOADER = 'UI_HIDE_LOADER';
