dc45fd85dc5b59772323fa50c87898dd9a001abb
Evgeniy Zaitsev <overhawlin@gmail.com>
2016-06-29 13:52:31 +0300

    remove redux dev tool from prod build

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -88,7 +88,7 @@ const createPersistentStore = compose(
     merge
   }),
   applyMiddleware(...middlewares),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
+  window.devToolsExtension && !IS_PROD ? window.devToolsExtension() : f => f
 )(createStore);
 
 export default (initialState = {}) => createPersistentStore(reducers, initialState);
