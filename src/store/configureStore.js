33205135d3fd1d6561e052c55c77c0527c3eec1d
Evgeniy Zaitsev <overhawlin@gmail.com>
2016-09-23 13:48:29 +0300

    add lodash

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,10 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import persistState from 'redux-localstorage';
+import assignIn from 'lodash/assignIn';
+import isObject from 'lodash/isObject';
 
-import reducers from '../reducers/index.js';
+import reducers from '../reducers/index';
 
 import authorize from '../middlewares/authorize';
 import player from '../middlewares/player';
@@ -23,10 +25,40 @@ const includeInLocalStorage = [
 	'authorize.expire'
 ];
 
-const include = (rule, state, initialState) => {
+export default initialState => {
+	const store = createStore(reducers, initialState, getEnhancer());
+
+	if (module.hot) {
+		module.hot.accept('../reducers', () =>
+			store.replaceReducer(require('../reducers').default)
+		);
+	}
+
+	return store;
+};
+
+function getEnhancer() {
+	let devTools = f => f;
+
+	if (!IS_PROD && window.devToolsExtension) {
+		devTools = window.devToolsExtension();
+	}
+
+	return compose(
+		applyMiddleware(...middlewares),
+		persistState('', {
+			key: 'vk-music',
+			slicer,
+			merge
+		}),
+		devTools
+	);
+}
+
+function include(rule, state, initialState) {
 	let newState = state;
 	let initState = initialState;
-	const keys = rule.split('.');
+	let keys = rule.split('.');
 
 	keys.forEach((key, index) => {
 		if (index === keys.length - 1) {
@@ -38,26 +70,32 @@ const include = (rule, state, initialState) => {
 
 		newState = newState[key];
 	});
-};
+}
 
-const slicer = () => state => {
-	let newState = {};
+function slicer() {
+	return state => {
+		let newState = {};
 
-	includeInLocalStorage.forEach(rule => {
-		include(rule, newState, state);
-	});
+		includeInLocalStorage.forEach(rule => {
+			include(rule, newState, state);
+		});
 
-	return newState;
-};
+		return newState;
+	};
+}
 
-const assignByRule = (rule, state, persistedState) => {
+function assignByRule(rule, state, persistedState) {
 	let newState = state;
 	let persistState = persistedState;
-	const keys = rule.split('.').slice(0, -1);
+	let keys = rule.split('.');
 
 	keys.forEach((key, index) => {
 		if (index === keys.length - 1) {
-			newState[key] = Object.assign({}, newState[key], persistState[key]);
+			if (isObject(newState[key])) {
+				newState[key] = assignIn({}, newState[key], persistState[key]);
+			} else {
+				newState[key] = persistState[key];
+			}
 		} else {
 			newState[key] = newState[key] || {};
 			persistState = persistState[key] || {};
@@ -65,30 +103,18 @@ const assignByRule = (rule, state, persistedState) => {
 
 		newState = newState[key];
 	});
-};
+}
 
-const merge = (initialState, persistedState) => {
+function merge(initialState, persistedState) {
 	if (!persistedState) {
 		return initialState;
 	}
 
-	let newState = Object.assign({}, initialState);
+	let newState = assignIn({}, initialState);
 
 	includeInLocalStorage.forEach(rule => {
 		assignByRule(rule, newState, persistedState);
 	});
 
 	return newState;
-};
-
-const createPersistentStore = compose(
-	persistState('', {
-		key: 'vk-music',
-		slicer,
-		merge
-	}),
-	applyMiddleware(...middlewares),
-	window.devToolsExtension && !IS_PROD ? window.devToolsExtension() : f => f
-)(createStore);
-
-export default (initialState = {}) => createPersistentStore(reducers, initialState);
+}
